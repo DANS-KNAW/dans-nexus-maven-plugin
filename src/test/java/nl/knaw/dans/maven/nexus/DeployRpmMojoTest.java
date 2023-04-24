43d66cbbac5d3dc6fcf5dc49259de716f300dfec
Jan van Mansum <janvanmansum@gmail.com>
2023-04-24 13:26:25 +0200

    Added stub test

---
+++ src/test/java/nl/knaw/dans/maven/nexus/DeployRpmMojoTest.java
@@ -0,0 +1,12 @@
+package nl.knaw.dans.maven.nexus;
+
+import org.junit.jupiter.api.Test;
+
+public class DeployRpmMojoTest {
+
+    @Test
+    public void testExecute() {
+        // Stub
+    }
+
+}
